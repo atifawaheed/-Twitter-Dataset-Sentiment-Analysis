87050ba958f97b5a1836167088c46bd45ca9072b
atifawaheed <atifawaheed789@gmail.com>
2026-03-31 12:39:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ with st.expander('Analyze Excel files'):
         df['analysis'] = df['score'].apply(analyze)
         st.write(df.head(10))
 
-        @st.cache
+        @st.cache_data
         def convert_df(df):
             # IMPORTANT: Cache the conversion to prevent computation on every rerun
             return df.to_csv().encode('utf-8')
